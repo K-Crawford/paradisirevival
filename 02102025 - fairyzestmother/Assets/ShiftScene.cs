090f81a48788bb2528fcb5d0fa3a0bf0d65e6d26
hazelwuhz <hazelwu@stanford.edu>
2025-02-26 19:37:33 -0800

    figured out minigame
    Lucas orz

---
+++ 02102025 - fairyzestmother/Assets/ShiftScene.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShiftScene : MonoBehaviour
+{
+
+    [Header("Grapefruit Radius Assignment")]
+    public float GrapefruitRadius;
+    
+    void ChangeSceneTo(string name) {
+        SceneManager.LoadScene(name);
+    }
+
+    float CalcDistToPlayer() {
+        return Vector3.Distance(transform.position, Camera.main.transform.position);
+    }
+
+    void Update() {
+        Debug.Log(CalcDistToPlayer());
+        if (CalcDistToPlayer() > GrapefruitRadius) {
+            ChangeSceneTo("main");
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, GrapefruitRadius);
+    }
+
+}
